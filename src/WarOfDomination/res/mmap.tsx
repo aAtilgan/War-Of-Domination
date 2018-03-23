3d27d8872fa6c04204af4e84513e7905e5657885
ayberktecimer <ayberktecimer@yahoo.com>
2018-03-24 02:16:55 +0300

    cleaning...

--- src/WarOfDomination/res/mmap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="mmap" tilewidth="32" tileheight="32" tilecount="1020" columns="34">
- <image source="MAP.png" width="1088" height="960"/>
-</tileset>
--- src/WarOfDomination/src/rpg/game/wfd/EnemyController.java
+++
@@ -1,5 +0,0 @@
-package rpg.game.tutorial;
-
-public class EnemyController {
-
-}
