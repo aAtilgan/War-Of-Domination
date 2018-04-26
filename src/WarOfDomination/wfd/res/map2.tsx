c2940abd9fc13eebe7ad1850ecf0bbef6352157c
ayberktecimer <ayberktecimer@yahoo.com>
2018-04-26 22:46:59 +0300

    Version 2 updates will be put here

---
+++ src/WarOfDomination/wfd/res/map2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="map2" tilewidth="32" tileheight="32" tilecount="756" columns="28">
+ <image source="map2.png" width="896" height="864"/>
+</tileset>
---
+++ src/WarOfDomination/wfd/res/map3_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="map3_tileset" tilewidth="32" tileheight="32" tilecount="1550" columns="50">
+ <image source="map3.png" width="1600" height="992"/>
+</tileset>
