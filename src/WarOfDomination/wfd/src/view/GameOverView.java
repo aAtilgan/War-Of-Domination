5a26302741945a3a5b1f4ad4350e5f2efd32d7b9
ayberktecimer <ayberktecimer@yahoo.com>
2018-04-29 11:58:19 +0300

    Gameover view updated and StoryView is added

--- src/WarOfDomination/wfd/src/view/GameOverView.java
+++ src/WarOfDomination/wfd/src/view/GameOverView.java
@@ -16,56 +16,62 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 import controller.GameManager;
+import model.RoundData;
 
-public class GameOverView extends BasicGameState{
+public class GameOverView extends BasicGameState {
 	private Sound game_over;
-	private int time=0;
-	private float alpha1 =0f;
+	private int time = 0;
+	private float alpha1 = 0f;
 	private boolean flag = true;
 	private float alpha2 = 0f;
 	Input input;
 	Image background;
-	int winner;
+	
+	String winner="";
 	public GameOverView(int gameover) {
 		// TODO Auto-generated constructor stub
-		
+
 	}
 
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
 		// TODO Auto-generated method stub
-		background=new Image("res/game_over_background.png");
+		RoundData.round=1;
+		background = new Image("res/game_over_background.png");
 		game_over = new Sound("res/game_over_sound.ogg");
 		alpha2 = 0f;
-		alpha1 =0f;
+		alpha1 = 0f;
 		flag = true;
-		
+		if (GameManager.hero_win > GameManager.enemy_win) {
+			winner="HEEEROO";
+		}else {
+			winner= "ENEMY";
+		}
 	}
 
 	@Override
-	public void render(GameContainer container, StateBasedGame arg1, Graphics g) throws SlickException 
-	{
-		
-		g.drawImage(background, 0,0, null);
+	public void render(GameContainer container, StateBasedGame arg1, Graphics g) throws SlickException {
+
+		g.drawImage(background, 0, 0, null);
 		g.setColor(new Color(1f, 1f, 1f, alpha1));
 		alpha1 = alpha1 + 0.02f;
-		//g.drawString("Hero Score: " + GameManager.hero_win, 100, 100); Kazanan gösterici
-		g.drawString("Press Enter to Return Main Menu" , 600, 500);
-		g.drawString("",
-				container.getScreenWidth()/2-150f , container.getScreenHeight()/2);
+		g.drawString("Hero Score is " + GameManager.hero_win, 100, 100);
+		g.drawString("Enemy Score is " + GameManager.enemy_win, 100, 150);
+		g.drawString("GAME WINNER IS: "+ winner, 100, 200);
+		// g.drawString("Hero Score: " + GameManager.hero_win, 100, 100); Kazanan
+		// gösterici
+		g.drawString("Press Enter to Return Main Menu", 600, 500);
+		g.drawString("", container.getScreenWidth() / 2 - 150f, container.getScreenHeight() / 2);
 	}
 
 	@Override
-	public void update(GameContainer gc, StateBasedGame sbg, int arg2) throws SlickException 
-	{
-		input= gc.getInput();
-		if(flag)
-		{
+	public void update(GameContainer gc, StateBasedGame sbg, int arg2) throws SlickException {
+		input = gc.getInput();
+		if (flag) {
 			game_over.play();
-			flag =false;
+			flag = false;
 		}
-		if(input.isKeyPressed(Input.KEY_ENTER))
-		{
+		if (input.isKeyPressed(Input.KEY_ENTER)) {
 			sbg.getState(0).init(gc, sbg);
 			sbg.enterState(0);
 		}
@@ -73,7 +79,7 @@ public class GameOverView extends BasicGameState{
 
 	@Override
 	public int getID() {
-		// TODO Auto-generated method stub
+
 		return 4;
 	}
 
