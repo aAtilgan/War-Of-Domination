1baf469fb18bd80d7d158748c03d9054c4a3731a
ayberktecimer <ayberktecimer@yahoo.com>
2018-04-29 11:59:08 +0300

    RoundData class is added
    it holds the transaction data between states

---
+++ src/WarOfDomination/wfd/src/model/RoundData.java
@@ -0,0 +1,19 @@
+package model;
+/**
+ * @author Ayberk
+ *
+ */
+public class RoundData {
+	public static int round=1;
+	
+	public static int character_choice_hero=-1;
+	public static int character_choice_enemy=-1;
+	
+	public static int weapon_choice_hero=-1;
+	public static int weapon_choice_enemy=-1;
+	
+	public static int choice_turn=0; // 0 stands for hero 1 stands for enemy
+	
+	public static int num_of_bullet_hero=0;
+	public static int num_of_bullet_enemy=0;
+}
