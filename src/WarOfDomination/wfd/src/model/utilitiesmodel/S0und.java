ac2dcce1b29803b984bb4034072c2ab4b09dd547
ayberktecimer <ayberktecimer@yahoo.com>
2018-05-05 22:34:53 +0300

    utilities model subsystem comment regulations
    Co-Authored-By: Akant Atılgan <atilgana@yahoo.com>

--- src/WarOfDomination/wfd/src/model/utilitiesmodel/S0und.java
+++ src/WarOfDomination/wfd/src/model/utilitiesmodel/S0und.java
@@ -1,4 +1,5 @@
 package model.utilitiesmodel;
+
 /**
  * @author Ayberk
  *
@@ -11,25 +12,35 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 
 public class S0und implements Observer {
+
+	// constants
 	private Music msc;
 	private Sound sound;
-	boolean on=true;
+
+	// variables
+	boolean on = true;
 	public float volume;
+
+	// Plays the music if music is on
 	public void playTitleMusic() throws SlickException {
-		msc=new Music("res/main_theme.ogg");
+		msc = new Music("res/main_theme.ogg");
 		if (on)
 			msc.loop();
 		volume = 1f;
 	}
+
+	// Changes the volume accordingly
 	public void changeVolume(float change) {
 		volume += change;
 		msc.setVolume(volume);
 	}
+
 	@Override
+	// Checks if volume in SettingsData class is changed. Applies changes if so.
 	public void update(Observable options, Object arg) {
 		// TODO Auto-generated method stub
 		if (options instanceof SettingsData) {
-			msc.setVolume((((SettingsData)options).volume)*0.01f);
+			msc.setVolume((((SettingsData) options).volume) * 0.01f);
 		}
 	}
 
--- src/WarOfDomination/wfd/src/model/utilitiesmodel/SettingsData.java
+++ src/WarOfDomination/wfd/src/model/utilitiesmodel/SettingsData.java
@@ -1,15 +1,18 @@
 package model.utilitiesmodel;
+
 /**
  * @author Ayberk
  *
  */
 public class SettingsData extends java.util.Observable {
-	// data
+
+	// variables
 	int volume;
 	int brightness;
 	boolean fullscreen;
 	boolean paused;
 
+	// contructor
 	public SettingsData() {
 		volume = 20;
 		brightness = 25;
@@ -20,27 +23,39 @@ public class SettingsData extends java.util.Observable {
 	public int getVolume() {
 		return volume;
 	}
+
+	// Changes the volume and notifys classes that needs to update according to this
+	// change
 	public void setVolume(int vol) {
-		if (vol>100) vol=100;
-		else if(vol<0) vol=0;
-		this.volume=vol;
+		if (vol > 100)
+			vol = 100;
+		else if (vol < 0)
+			vol = 0;
+		this.volume = vol;
 		setChanged();
 		notifyObservers();
 		clearChanged();
 	}
+
 	public int getBrightness() {
 		return brightness;
 	}
+
 	public void setBrightness(int br) {
-		if(br <0) br=0;
-		else if(br>100) br=0;
-		this.brightness=br;
+		if (br < 0)
+			br = 0;
+		else if (br > 100)
+			br = 0;
+		this.brightness = br;
 	}
+
 	public boolean isFullscreen() {
 		return fullscreen;
 	}
+
+	// Sets the game to fullscreen and notifys necessary classes.
 	public void setFullscreen(boolean fullscreen) {
-		this.fullscreen=fullscreen;
+		this.fullscreen = fullscreen;
 		setChanged();
 		notifyObservers();
 		clearChanged();
